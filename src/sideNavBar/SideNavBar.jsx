00f7a9adc3cb3163f23e71e4ac383cc0d2da2ae3
rohitjames77 <rohitjames77@outlook.com>
2026-01-16 14:40:04 +0530

    Fix main info styling

--- src/sideNavBar/SideNavBar.jsx
+++ src/sideNavBar/SideNavBar.jsx
@@ -57,7 +57,7 @@ useEffect(() => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-64 opacity-25 z-1">
+      <div className="flex items-center justify-center h-64 opacity-25 m-12">
         <div className="w-10 h-10 border-4 border-white border-t-transparent rounded-full animate-spin "></div>
       </div>
     );
@@ -71,21 +71,21 @@ useEffect(() => {
   }
 
   return (
-    <div className="border-2 row-start-1 row-end-21 col-start-1 col-end-19 opacity-50 rounded-r-lg border-r-3 border-r-white grid grid-rows-20 grid-col-20 ">
-      <nav id="side-nav-bar" className=" row-start-1 row-end-6 col-start-1 col-end-17 ">
+    <div className="border-2 row-start-1 row-end-21 col-start-1 col-end-19 opacity-50 rounded-r-lg border-r-3 border-r-white grid grid-rows-20 grid-col-20 md: ">
+      <nav id="side-nav-bar" className=" row-start-1 row-end-6 col-start-1 col-end-17 hover:border-b-2 hover:border-white  ">
         <div
           id="input-container"
-          className=" grid grid-rows-1 grid-cols-6 w-[90%] mt-2 ml-6 rounded-r-lg hover:border-b-2 hover:border-white "
+          className=" grid grid-rows-1 grid-cols-6 w-[90%] mt-2 ml-6 rounded-lg hover:border-2 hover:border-gray-300  "
         >
           <input
             type="text"
             onChange={handleInputValue}
-            className=" row-start-1 col-start-1 col-end-6 placeholder-white focus:border-none focus:outline-none focus:shadow-none placeholder:text-xs text-white text-sm"
+            className=" row-start-3 row-end-5 col-start-3 col-end-6 placeholder-white focus:border-none focus:outline-none focus:shadow-none placeholder:text-xs text-white text-sm "
             placeholder="New Delhi"
           ></input>
           <span
             id="search-icon-container"
-            className="rounded-r-lg text-white col-start-6 col-end-7 hover:border-2 hover:border-white active:translate-y-[2px] active:bg-white active:text-black"
+            className="rounded-r-lg text-white row-start-3 col-start-6 col-end-7 hover:border-2 hover:border-white active:translate-y-[2px] active:bg-white active:text-black"
             onClick={handleInputSubmit}
           >
             <FiSearch className="text-xs relative top-1" />
@@ -95,13 +95,13 @@ useEffect(() => {
       </nav>
        
        <div id="info-parent-container" className="row-start-2 row-end-19 col-start-1 col-end-17 border-4 grid grid-rows-20 grid-cols-20">
-<h1 className="row-start-1 row-end-2 col-start-1 col-end-6 text-white text-4xl md:text-4xl lg:text-6xl">{isinputSubmited}</h1>
-        <h1 className="row-start-2 row-end-3 col-start-1 col-end-6 text-white text-4xl md:text-4xl lg:text-6xl">{Math.round((data.currentConditions.temp-32)*5/9)}*C</h1>
-        <p className="row-start-4 col-start-2 col-end-16  text-white md:text-2xl lg:text-4xl">Wind Speed : {data.currentConditions.windspeed}mph</p>
-       <p className="row-start-5 col-start-2 col-end-6  text-white text-3xl md:text-2xl lg:text-4xl">{data.currentConditions.conditions}</p>
-       <p className="row-start-5 col-start-2 col-end-6  text-white text-3xl md:text-2xl lg:text-4xl">{data.currentConditions.conditions}</p>
-       <p className="row-start-7 col-start-2 col-end-17  text-white text-3xl md:text-2xl lg:text-4xl">UV Index :{data.currentConditions.uvindex}</p>
-       
+<h1 className="row-start-1 row-end-5 col-start-9 col-end-15 text-white text-4xl md:text-6xl lg:text-6xl">{isinputSubmited}</h1>
+        <h1 className="row-start-3 row-end-5 col-start-8 col-end-16 text-white text-6xl md:text-4xl lg:text-6xl">{Math.round((data.currentConditions.temp-32)*5/9)}*C</h1>
+        <h2 className="row-start-6 col-start-6 col-end-18 text-2xl text-white md:text-2xl lg:text-4xl">Wind Speed : {data.currentConditions.windspeed}mph</h2>
+       <h2 className="row-start-8 col-start-4 col-end-18  text-white text-2xl md:text-2xl lg:text-4xl">Conditions:{data.currentConditions.conditions}</h2>
+        <h2 className="row-start-12 col-start-7 col-end-6  text-white text-3xl md:text-3xl lg:text-4xl">Humidity:{data.currentConditions.humidity}%</h2>
+       <h2 className="row-start-10 col-start-8 col-end-17  text-white text-3xl md:text-3xl lg:text-4xl">UV Index :{data.currentConditions.uvindex}</h2>
+       <div className="border-2 border-white rounded-3xl row-start-14 row-end-16 col-start-7 col-end-18"></div>
        
        </div>
        
