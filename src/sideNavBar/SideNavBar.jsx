d0a5b116fed0be83eb10e5d0a28969d34efb7899
rohitjames77 <rohitjames77@outlook.com>
2026-01-16 15:55:19 +0530

    Fix style responsiveness

--- src/sideNavBar/SideNavBar.jsx
+++ src/sideNavBar/SideNavBar.jsx
@@ -3,56 +3,54 @@ import { FiSearch } from "react-icons/fi";
 // import { useOutletContext } from "react-router";
 import axios from "axios";
 import { useState, useEffect } from "react";
-function SideNavBar({ inputValue, setInputValue,data,setData }) {
+function SideNavBar({ inputValue, setInputValue, data, setData }) {
   const [isinputSubmited, setInputSubmit] = useState("");
   const handleInputValue = (event) => {
-    if(event.target.value != ""){
+    if (event.target.value != "") {
       setInputValue(event.target.value);
-    } else{
-      setInputValue(event.target.placeholder)
+    } else {
+      setInputValue(event.target.placeholder);
     }
   };
 
   const handleInputSubmit = () => {
     console.log(inputValue);
-    
+
     setInputSubmit(inputValue);
-    
   };
   console.log(isinputSubmited);
 
-  
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
-const location = isinputSubmited?.trim() || "newdelhi";
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      setError(null);
+  const location = isinputSubmited?.trim() || "newdelhi";
 
-      const response = await axios.get(
-        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline",
-        {
-          params: {
-            location,
-            key: "NTV85PXGEFFBCHC42DLFKNLY6",
-          },
-        }
-      );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
 
-      setData(response.data);
-    } catch (err) {
-      setError(err.response?.data?.message || err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+        const response = await axios.get(
+          "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline",
+          {
+            params: {
+              location,
+              key: "NTV85PXGEFFBCHC42DLFKNLY6",
+            },
+          }
+        );
 
-  fetchData();
-}, [location]); 
+        setData(response.data);
+      } catch (err) {
+        setError(err.response?.data?.message || err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [location]);
   console.log(data);
 
   if (loading) {
@@ -71,49 +69,59 @@ useEffect(() => {
   }
 
   return (
-    <div className="border-2 row-start-1 row-end-21 col-start-1 col-end-19 opacity-50 rounded-r-lg border-r-3 border-r-white grid grid-rows-20 grid-col-20 md: ">
-      <nav id="side-nav-bar" className=" row-start-1 row-end-6 col-start-1 col-end-17 hover:border-b-2 hover:border-white  ">
+    <div className="row-start-1 row-end-21 col-start-1 col-end-21 rounded-r-lg border-r-3 border-r-white grid grid-rows-20 grid-col-20 z-1 ">
+      <nav
+        id="side-nav-bar"
+        className=" row-start-1 row-end-6 col-start-1 col-end-17 hover:border-b-2 hover:border-white  "
+      >
         <div
           id="input-container"
-          className=" grid grid-rows-1 grid-cols-6 w-[90%] mt-2 ml-6 rounded-lg hover:border-2 hover:border-gray-300  "
+          className=" grid grid-rows-1 grid-cols-6 w-[90%] mt-2 ml-6 rounded-lg  "
         >
           <input
             type="text"
             onChange={handleInputValue}
-            className=" row-start-3 row-end-5 col-start-3 col-end-6 placeholder-white focus:border-none focus:outline-none focus:shadow-none placeholder:text-xs text-white text-sm "
+            className=" hover:border-b-2 hover:border-gray-300  row-start-3 row-end-5 col-start-3 col-end-6 placeholder-white focus:border-none focus:outline-none focus:shadow-none placeholder:text-lg text-white text-sm "
             placeholder="New Delhi"
           ></input>
           <span
             id="search-icon-container"
-            className="rounded-r-lg text-white row-start-3 col-start-6 col-end-7 hover:border-2 hover:border-white active:translate-y-[2px] active:bg-white active:text-black"
+            className="rounded-r-lg text-white row-start-3 col-start-6 col-end-7 active:translate-y-[2px] active:text-black gap-y-2"
             onClick={handleInputSubmit}
           >
-            <FiSearch className="text-xs relative top-1" />
+            <FiSearch className="text-xl relative top-1" />
           </span>
         </div>
-
       </nav>
-       
-       <div id="info-parent-container" className="row-start-2 row-end-19 col-start-1 col-end-17 border-4 grid grid-rows-20 grid-cols-20">
-<h1 className="row-start-1 row-end-5 col-start-9 col-end-15 text-white text-4xl md:text-6xl lg:text-6xl">{isinputSubmited}</h1>
-        <h1 className="row-start-3 row-end-5 col-start-8 col-end-16 text-white text-6xl md:text-4xl lg:text-6xl">{Math.round((data.currentConditions.temp-32)*5/9)}*C</h1>
-        <h2 className="row-start-6 col-start-6 col-end-18 text-2xl text-white md:text-2xl lg:text-4xl">Wind Speed : {data.currentConditions.windspeed}mph</h2>
-       <h2 className="row-start-8 col-start-4 col-end-18  text-white text-2xl md:text-2xl lg:text-4xl">Conditions:{data.currentConditions.conditions}</h2>
-        <h2 className="row-start-12 col-start-7 col-end-6  text-white text-3xl md:text-3xl lg:text-4xl">Humidity:{data.currentConditions.humidity}%</h2>
-       <h2 className="row-start-10 col-start-8 col-end-17  text-white text-3xl md:text-3xl lg:text-4xl">UV Index :{data.currentConditions.uvindex}</h2>
-       <div className="border-2 border-white rounded-3xl row-start-14 row-end-16 col-start-7 col-end-18"></div>
-       
-       </div>
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+
+      <div
+        id="info-parent-container"
+        className="row-start-2 row-end-19 col-start-1 col-end-17 grid grid-rows-20 grid-cols-20"
+      >
+        <h1 className="row-start-1 row-end-5 col-start-9 col-end-15 text-white text-4xl md:text-6xl lg:text-6xl xl:text-4xl">
+          {isinputSubmited}
+        </h1>
+        <h1 className="row-start-3 row-end-7 col-start-9 col-end-16 text-white text-6xl md:text-6xl lg:text-8xl xl:text-4xl">
+          {Math.round(((data.currentConditions.temp - 32) * 5) / 9)}*
+        </h1>
+        <h2 className="row-start-6 col-start-6 col-end-18 text-2xl text-white md:text-2xl lg:text-4xl xl:text-4xl">
+          Wind Speed : {data.currentConditions.windspeed}mph
+        </h2>
+        <h2 className="row-start-7 mt-2 col-start-6 col-end-20 text-white text-2xl md:text-2xl lg:text-4xl xl:text-4xl">
+          Conditions:{data.currentConditions.conditions}
+        </h2>
+
+        <h2 className=" rounded-3xl row-start-8 row-end-10 mt-2 col-start-6 col-end-18 flex-1 text-white text-2xl md:text-3xl lg:text-4xl xl:text-4xl flex ">
+          Humidity:{data.currentConditions.humidity}%
+        </h2>
+
+        <h2 className=" rounded-3xl row-start-11 row-end-12 col-start-6 col-end-18 text-white text-2xl md:text-3xl lg:text-4xl xl:text-4xl ml-14 ">
+          UV Index :{data.currentConditions.uvindex}
+        </h2>
+        <div className="border-2 border-white rounded-3xl row-start-12 row-end-14 col-start-5 col-end-18 "></div>
+        <div className="border-2 border-white rounded-3xl row-start-15 row-end-17 col-start-2 col-end-20 "></div>
+        <div className="border-2 border-white rounded-3xl row-start-18 row-end-21 col-start-1 col-end-21 "></div>
+      </div>
     </div>
   );
 }
