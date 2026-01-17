7ee8f9096b1db9b5dbcd62b6e6cd92dcf4b008f5
rohitjames77 <rohitjames77@outlook.com>
2026-01-17 11:57:38 +0530

    Configure routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router'
-import SideNavBar from './sideNavBar/SideNavBar.jsx'
-import ContentPage from './content/ContentPage.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import ContentPage from "./content-page/ContentPage.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "content-page",
+        element: <ContentPage />,
+      },
+    ],
+  },
+]);
 
-
-// const router = createBrowserRouter([
-//   {path :"/",
-//     element:<App/>,
-//   children:[
-//     {
-//       path:"navbar",
-//       element:<SideNavBar/>
-//     },
-//     {
-//       path:"content",
-//       element:<ContentPage/>
-//     }
-//   ]
-//   }
-// ])
-
-
-// <RouterProvider router = {router}/>
-
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App/>
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
